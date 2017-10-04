65be79de62854f59b2d5f506d216371addf4c355
Vsevolod Parshin <seva.parshin@yandex.ru>
2017-10-04 22:20:21 +0300

    Initial commit

---
+++ TemaParser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace TemaParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uriToDownload = @"https://www.artlebedev.ru/kovodstvo/business-lynch/tema/";
+            getContent(uriToDownload);
+        }
+
+        static HtmlDocument getContent(string uri)
+        {
+            HtmlWeb web = new HtmlWeb();
+            return web.Load(uri);
+        }
+    }
+}
