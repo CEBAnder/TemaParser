264b3ae8de52d394f8b7c9464d4e239c308ae4a6
Vsevolod Parshin <seva.parshin@yandex.ru>
2017-10-06 00:06:32 +0300

    Added basic functions

--- TemaParser/Program.cs
+++ TemaParser/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace TemaParser
 {
@@ -12,13 +14,123 @@ namespace TemaParser
         static void Main(string[] args)
         {
             string uriToDownload = @"https://www.artlebedev.ru/kovodstvo/business-lynch/tema/";
-            getContent(uriToDownload);
+            var reviewsList = new List<Review>();
+            var reviewsPage = getContent(uriToDownload);
+            HtmlNodeCollection reviewLinks = reviewsPage.DocumentNode.SelectNodes("//td/div/span | //td/div/a");
+            //for (int i = 0; i < reviewLinks.Count; i += 2)
+            for (int i = 0; i < 20; i += 2)
+            {
+                try
+                {
+                    reviewsList.Add(new Review { date = reviewLinks[i].InnerHtml.ToString() });
+                    reviewsList[reviewsList.Count - 1].link = "https://www.artlebedev.ru" + reviewLinks[i + 1].OuterHtml.ToString().Split('"')[1];
+                    var reviewPage = getContent(reviewsList[reviewsList.Count - 1].link);
+                    reviewsList[reviewsList.Count - 1].comments = getReviewComments(reviewPage);
+                    reviewsList[reviewsList.Count - 1].text = getReviewText(reviewPage);
+                    Console.WriteLine("Обратотано " + (i / 2 + 1) + " записей!");
+                }
+                catch
+                {
+                    reviewsList.Remove(reviewsList[reviewsList.Count - 1]);//удаление некорректной записи из листа
+                }
+            }
+            Console.WriteLine("Лист сформирован!");
+            Review.saveToJson(reviewsList);
+            Console.WriteLine("Файл готов!");
+            Console.ReadKey();
         }
 
+        //получение html-страницы по указанному uri
         static HtmlDocument getContent(string uri)
         {
             HtmlWeb web = new HtmlWeb();
             return web.Load(uri);
+        }
+
+        //получение всех комментариев Тёмы
+        static List<string> getReviewComments(HtmlDocument hd)
+        {
+            var list = new List<string>();
+            HtmlNodeCollection comments = hd.DocumentNode.SelectNodes("//div[contains(@class, 'LynchComment')]");
+            foreach(HtmlNode hn in comments)
+            {
+                list.Add(hn.InnerText);
+            }
+            return list;
+        }
+
+        //получение сопроводительного текста
+        static string getReviewText(HtmlDocument hd)
+        {
+            return hd.DocumentNode.SelectNodes("//div[contains(@class, 'value')]")[1].InnerText;
+        }
+    }
+
+    class Review
+    {
+        public string text { get; set; }
+        public string date { get; set; }
+        public string link { get; set; }
+        public List<string> comments { get; set; }
+
+        private string getReviewString()
+        {
+            try
+            {
+                string stringToReturn = "";
+                stringToReturn += this.date.ToString() + '\t' + this.text + '\t' + this.link;
+                foreach(string rc in comments)
+                {
+                    string stringToAdd = "";
+                    stringToAdd = "\n\r" + rc;
+                    stringToReturn += stringToAdd;
+                }
+                return stringToReturn.Replace("&nbsp;", " ");
+            }
+            catch
+            {
+                return "Битая ссылка";
+            }
+        }
+
+        private string getReviewStringOnlyTema()
+        {
+            try
+            {                
+                string stringToReturn = "";
+                stringToReturn += this.link;
+                foreach (string rc in comments)
+                {
+                    string stringToAdd = "";
+                    stringToAdd = "\n\r" + rc;
+                    stringToReturn += stringToAdd;
+                }                
+                return stringToReturn.Replace("&nbsp;", " ").Replace("&laquo;", "").Replace("&raquo;", "");
+            }
+            catch
+            {
+                return "Небогоугодная ссылка";
+            }
+        }
+
+        public static void saveReviewsToFile(List<Review> listOfReviews)
+        {
+            using (StreamWriter sw = new StreamWriter("ParseResult.txt"))
+            {
+                foreach(Review review in listOfReviews)
+                {                   
+                    sw.WriteLine(review.getReviewStringOnlyTema());
+                }
+            }
+        }
+
+        public static void saveToJson(List<Review> listOfReviews)
+        {
+            var js = JsonConvert.SerializeObject(listOfReviews);
+            using (StreamWriter sw = new StreamWriter("ParseResult.json"))
+            {
+                sw.WriteLine(js);
+            }
         }
     }
 }
